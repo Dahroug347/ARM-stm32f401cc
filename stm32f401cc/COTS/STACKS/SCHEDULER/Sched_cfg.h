884d9b3fa23654a7bb1dcb99a97e5570acd593fc
DESKTOP-J0IJQH0\dahroug <mahmoud.dahroug7@gmail.com>
2022-05-01 02:30:06 +0200

    modifying sched_cfg

--- stm32f401cc/COTS/STACKS/SCHEDULER/Sched_cfg.h
+++ stm32f401cc/COTS/STACKS/SCHEDULER/Sched_cfg.h
@@ -11,7 +11,7 @@
 /* configured parameters defined macros section */
 #define 	SCHED_TASKS_NUM			10
 #define		SCHED_PRORITY			SCHED_PRIORITY_OFF
-#define 	SCHED_TARGET			ATMEGA_32
+#define 	SCHED_TARGET			STM_32_M4
 #define 	SCHED_TICK				1
 
 #endif 		/* SCHED_CFG_H_ */
